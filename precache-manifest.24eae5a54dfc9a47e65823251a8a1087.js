3491d916ed08353d389d35478d3a1d5785d9d9c2
vivek90s <viv281999@gmail.com>
2021-02-09 23:57:00 +0530

    Updates

---
+++ precache-manifest.24eae5a54dfc9a47e65823251a8a1087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118a45e62d61d9b7df8be4c8ca65e086",
+    "url": "/user-profile/index.html"
+  },
+  {
+    "revision": "ec9468901ca4262aa403",
+    "url": "/user-profile/static/css/main.9a8362cb.chunk.css"
+  },
+  {
+    "revision": "f554f317bf388566fcce",
+    "url": "/user-profile/static/js/2.d13672f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-profile/static/js/2.d13672f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9468901ca4262aa403",
+    "url": "/user-profile/static/js/main.9ff7f903.chunk.js"
+  },
+  {
+    "revision": "61fd91592779dd11031c",
+    "url": "/user-profile/static/js/runtime-main.591b0f5c.js"
+  }
+]);
